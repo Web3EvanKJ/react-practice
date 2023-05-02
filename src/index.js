4dfff4f096f80b312688cdddf8245e5c26755844
Web3EvanKJ <evan.keanej@gmail.com>
2023-05-02 15:23:41 +0700

    New Features and Form

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import "./styles.css";
 // };
 
 // Preferred to use above way
-// Capital in function name, return something(html), always return 1 parent element, use section/article, use <> </>, camelcase in HTML, always self-close html tags even thought the tags dont need closing tags
+// Capital in function name, return something(html), always return 1 parent element, use section/article, use <h2> </>, camelcase in HTML, always self-close html tags even thought the tags dont need closing tags
 
 // const Greeting = function () {
 //   return React.createElement("h2", {}, "Hello World");
@@ -39,40 +39,89 @@ function Greeting() {
   );
 }
 
+const books = [
+  {
+    author: "Wendy Loggia",
+    title: "Taylor Swift: A Little Golden Book Biography",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81X1plzH9DL._AC_UL900_SR900,600_.jpg",
+    id: 1,
+  },
+  {
+    author: "Ramit Sethi",
+    title:
+      "I Will Teach You to Be Rich: No Guilt. No Excuses. Just a 6-Week Program That Works (Second Edition)",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71zwHcw-D7L._AC_UL900_SR900,600_.jpg",
+    id: 2,
+  },
+];
+
+// const names = ["john", "peter", "susan"];
+// const NeatNames = names.map((name) => {
+//   return <h1>{name}</h1>;
+// });
+// console.log(NeatNames);
+
+const Book = ({ img, title, author }) => {
+  return (
+    <section className="book">
+      <EventExamples />
+      <img src={img} alt={title}></img>
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {/* <p>{let x = 6}</p> Not an expression */}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("Handle Form Input");
+  };
+  const handleButtonClick = () => {
+    alert("Handle Form Input");
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click Me</button>
+    </section>
+  );
+};
+
 // Book List
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81X1plzH9DL._AC_UL900_SR900,600_.jpg"
-    alt="Taylor Swift: A Little Golden Book Biography "
-  />
-);
-const Title = () => <h2>Taylor Swift: A Little Golden Book Biography </h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5em",
-  };
-  return <h4 style={inlineHeadingStyles}>Wendy Loggia</h4>;
-};
-
-const Book = () => {
-  return (
-    <section className="book">
-      <Image />
-      <Title />
-      <Author />
+      {/* <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+          commodi repellat quis consequatur quae officiis hic temporibus
+          consectetur nostrum laboriosam culpa ipsam inventore doloribus libero
+          voluptatem perspiciatis, id quibusdam incidunt?
+        </p>
+        <button>Click Me</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      /> */}
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
